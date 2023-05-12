71d7c89cf3f356e9e8960e86d8e73207d5bffe4e
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2023-05-12 14:56:47 +0100

    allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "1"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 # Application definition
